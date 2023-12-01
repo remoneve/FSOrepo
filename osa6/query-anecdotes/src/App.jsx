9f5fcacccb69168f1dbf40cb4c63ec6837cd9d66
remoneve <remonenveeti@gmail.com>
2023-12-01 19:01:52 +0200

    6.20

--- osa6/query-anecdotes/src/App.jsx
+++ osa6/query-anecdotes/src/App.jsx
@@ -1,19 +1,30 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
-const App = () => {
+import { useQuery } from '@tanstack/react-query'
+import { getAnecdotes } from './requests'
 
+const App = () => {
   const handleVote = (anecdote) => {
     console.log('vote')
   }
 
-  const anecdotes = [
-    {
-      "content": "If it hurts, do it more often",
-      "id": "47145",
-      "votes": 0
-    },
-  ]
+  const result = useQuery({
+    queryKey: ['anecdotes'],
+    queryFn: getAnecdotes,
+    retry: 1
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
+
+  if ( result.isLoading ) {
+    return <div>loading data...</div>
+  }
+
+  if ( result.isError ) {
+    return <div>anecdote service not available due to problems in server</div>
+  }
+
+  const anecdotes = result.data
 
   return (
     <div>
--- osa6/query-anecdotes/src/components/AnecdoteForm.jsx
+++ osa6/query-anecdotes/src/components/AnecdoteForm.jsx
@@ -1,10 +1,8 @@
 const AnecdoteForm = () => {
-
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
 }
 
   return (
