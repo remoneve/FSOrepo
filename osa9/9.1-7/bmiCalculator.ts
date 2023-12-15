b615ea551b326f5752006353afc5b1c978ddd194
remoneve <remonenveeti@gmail.com>
2023-12-15 17:52:57 +0200

    9.2-3

--- osa9/9.1-7/bmiCalculator.ts
+++ osa9/9.1-7/bmiCalculator.ts
@@ -1,11 +1,37 @@
-const calculateBmi = (height: number, weight: number) => {
-  const bmi = weight/Math.pow(height/100, 2)
-
-  if (bmi < 18.5) return 'Underweight (low weight)'
-  if (bmi > 18.5 && bmi < 25 ) return 'Normal (healthy weight)'
-  if (bmi > 25 ) return 'Overweight (high weight)'
+interface BmiValues {
+  height: number;
+  weight: number;
 }
 
-console.log(calculateBmi(180, 74))
-console.log(calculateBmi(180, 59))
-console.log(calculateBmi(180, 102))
+const calculateBmi = (height: number, weight: number) => {
+  const bmi = weight/Math.pow(height/100, 2);
+
+  if (bmi < 18.5) return 'Underweight (low weight)';
+  if (bmi > 18.5 && bmi < 25 ) return 'Normal (healthy weight)';
+  if (bmi > 25 ) return 'Overweight (high weight)';
+}
+
+const parseMeasurements = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments!')
+  if (args.length > 4) throw new Error('Too many arguments!')
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    }
+  } else {
+    throw new Error('Provided values are not numbers!')
+  }
+}
+
+try {
+  const { height, weight } = parseMeasurements(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (error: unknown) {
+  let errorMessage = '';
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+  console.log(errorMessage + ' (should be 2)');
+}
---
+++ osa9/9.1-7/exerciseCalculator.ts
@@ -0,0 +1,97 @@
+interface Result {
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
+}
+
+interface Values {
+  hours: number[];
+  target: number;
+}
+
+type Operation = 'trainingDays' | 'rating';
+
+const calculateExercises = (hours: number[], target: number): Result => {
+  const calcAvg = (hours: number[]) => {
+    var avg = 0;
+    for (var i = 0; i < hours.length; i++) {
+      avg += hours[i];
+    }
+    return avg / hours.length;
+  }
+
+  const calculator = (hours: number[], target: number, op: Operation) => {
+    switch(op) {
+      case 'trainingDays':
+        var days = 0
+        for (var i = 0; i < hours.length; i++) {
+          if (hours[i] > 0) days += 1;
+        }
+        return days;
+      case 'rating':
+        const average = calcAvg(hours);
+        if (average > target) return 3;
+        else if (average < target && average > target - 1) return 2;
+        else if (average < target - 1) return 1;
+      }
+    }
+    
+  const setSuccess = (hours: number[], target: number): boolean => {
+    const average = calcAvg(hours);
+    if (average > target) return true;
+    else return false;
+  }
+
+  const setRatingDesc = (rating: number): string => {
+    switch(rating) {
+      case 1:
+        return 'poor';
+      case 2:
+        return 'great';
+      case 3:
+        return 'excellent';
+    }
+  }
+
+  return {
+    periodLength: hours.length,
+    trainingDays: calculator(hours, target, 'trainingDays'),
+    success: setSuccess(hours, target),
+    rating: calculator(hours, target, 'rating'),
+    ratingDescription: setRatingDesc(calculator(hours, target, 'rating')),
+    target: target,
+    average: calcAvg(hours)
+  }
+}
+
+const parseExcercises = (args: string[]): Values => {
+  if (args.length < 4) throw new Error('Not enough arguments!')
+
+  if (!isNaN(Number(args[2]))) {
+    var hourList = []
+    for (var i = 3; i < args.length; i++) {
+      if (isNaN(Number(args[i]))) throw new Error('Arguments need to be numbers!')
+      hourList.push(Number(args[i]))
+    }
+
+    return {
+      target: Number(args[2]),
+      hours: hourList,
+    }
+  }
+}
+
+try {
+  const { hours, target } = parseExcercises(process.argv);
+  console.log(calculateExercises(hours, target));
+} catch (error: unknown) {
+  let errorMessage = '';
+  if (error instanceof Error) {
+    errorMessage = error.message;
+  }
+  console.log(errorMessage);
+}
