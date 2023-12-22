7429d04ade1e9b8aa25c78b714b2e6d408f82839
remoneve <remonenveeti@gmail.com>
2023-12-22 09:58:37 +0200

    9.11

--- osa9/patientor_backend/index.ts
+++ osa9/patientor_backend/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import diagnosisRouter from './src/routes/diagnosesRouter';
 import patientRouter from './src/routes/patientRouter';
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 const PORT = 3001;
 
